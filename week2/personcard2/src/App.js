101d9f08c97a439f1a454f63bb3c6bda17eb21a3
jcochran206 <jonathan.o.cochran@gmail.com>
2022-06-15 18:51:55 -0700

    updated personcard to use personAge and updated handleclick event in app

--- week2/personcard2/src/App.js
+++ week2/personcard2/src/App.js
@@ -5,40 +5,33 @@ import PersonCard from '../src/components/Personcard/Personcard';
 
 function App() {
 
-  const person = 
+  const personArr = [
   { 
     id: 1,
     firstName: "Steph",
     lastName: "Curry",
-    age: " ",
+    age: 30,
     hairColor: "black",
-  }
-
-  const person2 =  { 
+  },
+  { 
     id: 2,
     firstName: "Klay",
     lastName: "Thompson",
     age: 32,
     hairColor: "black",
   }
+  ]
   
   return (
     <div className="App">
-      
-        <PersonCard key={person.id}
+        {personArr.map((person) => (
+          <PersonCard key={person.id}
           firstName={person.firstName}
           lastName={person.lastName}
           age={person.age}
           hairColor={person.hairColor} 
         />  
-
-        <PersonCard key={person2.id}
-          firstName={person2.firstName}
-          lastName={person2.lastName}
-          age={person2.age}
-          hairColor={person2.hairColor} 
-        /> 
-     
+        ))}
     </div>
   );
 }
--- week2/personcard2/src/components/Personcard/Personcard.jsx
+++ week2/personcard2/src/components/Personcard/Personcard.jsx
@@ -2,24 +2,21 @@ import React, {useState} from 'react';
 import '../Personcard/Personcard.css';
 
 const Personcard = (props) => {
-    const [personAge, setAge] = useState({
-        age: 28
-    })
-
     const {firstName, lastName, age, hairColor } = props;
+    const [personAge, setAge] = useState(age)
+
+    
 
     const handleClick = () => {
-        setAge({
-            age: personAge.age + 1
-        });
+        setAge( personAge + 1);
     }
 
   return (
     <div className='card'>
         <h1 className='bold-text'>{lastName}, {firstName}</h1>
-        <p className='p-text'>Age: {personAge.age}</p>
+        <p className='p-text'>Age: {personAge}</p>
         <p className='p-text'>Hair Color: {hairColor}</p>
-        <button className='btn' onClick={e => handleClick()}>Birthday button for : {props.firstName} {props.lastName}</button>
+        <button className='btn' onClick={e => handleClick()}>Birthday button for : {firstName} {lastName}</button>
     </div>
   )
 }
