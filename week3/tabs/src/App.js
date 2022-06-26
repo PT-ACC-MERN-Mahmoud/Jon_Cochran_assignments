b711d2ac0f7dcc6bdde8cc44f73868eb1934192f
jcochran206 <jonathan.o.cochran@gmail.com>
2022-06-26 13:50:44 -0700

    static tabs and content

--- week3/tabs/src/App.js
+++ week3/tabs/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import Tab from '../src/components/Tabs/Tab';
-import '../src/components/Tabs/Tab.css';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Tab/>
+      <Tab />
     </div>
   );
 }
--- week3/tabs/src/components/Tabs/Tab.jsx
+++ week3/tabs/src/components/Tabs/Tab.jsx
@@ -1,18 +1,40 @@
 import React, {useState} from 'react';
+import '../Tabs/Tab.css';
 
 const Tab = (props) => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const handleToggle = (idx) => {
+    setToggleState(idx)
+  }
+    
   return (
     <div className="container">
-        {/* buttons */}
+       
         <div className="bloc-tabs">
-            <button className="tablinks">Tab 1</button>
+            <button className={toggleState === 1 ? 'tabs active': 'tabs'} onClick={() => handleToggle(1)}>Tab 1</button>
+            <button className={toggleState === 2 ? 'tabs active': 'tabs'} onClick={() => handleToggle(2)}>Tab 2</button>
         </div>
-        {/* tab content */}
-        <div id="tab" className="tabs">
-            <h3>Tab One</h3>
+        
+        <div className="content-tabs">
+        <div id="tab" className={toggleState === 1 ? 'content active-content' : 'content'}>
+            <h3>Content One</h3>
             <hr/>
-            <p>Tab One Content</p>
+            <p> 
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, nihil.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque repellendus corrupti corporis dolor culpa commodi!
+            </p>
         </div>
+
+        <div id="tab" className={toggleState === 2 ? 'content active-content' : 'content'}>
+            <h3>Content Two</h3>
+            <hr/>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, content 2.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque repellendus corrupti corporis dolor culpa commodi!
+            </p>
+        </div>
+        </div>
+        
     </div>
   )
 }
