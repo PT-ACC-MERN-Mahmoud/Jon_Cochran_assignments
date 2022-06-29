a48402a90742406ffbb1cdf8c708669a78a483c6
jcochran206 <jonathan.o.cochran@gmail.com>
2022-06-28 22:00:38 -0700

    fixed axios call by adding .results

--- week4/axiospokemonapi/src/App.js
+++ week4/axiospokemonapi/src/App.js
@@ -6,14 +6,14 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    const url = 'https://pokeapi.co/api/v2/pokemon?limit=807&offset=0';
-    axios.get(url)
-    .then(res=>{setPokemons(res.data)})
-    .catch(err => console.log(err))
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+      .then(res => setPokemons(res.data.results) )
+      .catch(err => console.log(err))
   }, []);
 
   return (
     <div className="App">
+      <h1> Axios Pokemon API </h1>
       {pokemons.map((pokemon, idx)=>(
         <div key={idx}>
           {pokemon.name}
