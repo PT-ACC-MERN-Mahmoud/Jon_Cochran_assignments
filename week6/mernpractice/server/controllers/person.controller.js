9bc4ecff08e2fa936e1ab52e7caaf7abc2a4fdc7
jcochran206 <jonathan.o.cochran@gmail.com>
2022-07-16 17:34:07 -0700

    modified model to create Person, made route, and tested connection to database

--- week6/mernpractice/server/controllers/person.controller.js
+++ week6/mernpractice/server/controllers/person.controller.js
@@ -1,8 +1,13 @@
-const { model } = require("mongoose");
-
-model.exports.index = (req, res) => {
+const Person = require('../models/person.model')
+module.exports.index = (request, response) => {
     response.json({
         message: "Hello World"
     });
 }
+
+module.exports.createPerson = (request, response) => {
+    Person.create(request.body)
+        .then(person=>response.json(person))
+        .catch(err=>console.log(err));
+} 
 
--- week6/mernpractice/server/server.js
+++ week6/mernpractice/server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
+require('./config/mongoose.config')
 require('./routes/person.routes')(app);
 
 const PORT = 8000;
